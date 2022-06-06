e1678292efe9fc89134aa32129131ddd6537f9b1
JoseTorquato <jose.torquato@grupoboticario.com.br>
2022-06-06 17:00:43 -0300

    feat: create entitie person

---
+++ src/models/entities/person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name, age, district, profession) -> None:
+        self.name = name
+        self.age = age
+        self.district = district
+        self.profession = profession
