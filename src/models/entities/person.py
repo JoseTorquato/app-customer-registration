658a66ea12d33a5dc6a31c5c94109c6bed1ff2ad
JoseTorquato <jltorquato12@gmail.com>
2022-06-06 21:26:15 -0300

    feat: add type hints

--- src/models/entities/person.py
+++ src/models/entities/person.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, name, age, district, profession) -> None:
+    def __init__(self, name:str, age:int, district:str, profession:str) -> None:
         self.name = name
         self.age = age
         self.district = district
