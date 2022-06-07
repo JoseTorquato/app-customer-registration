c82b2c6979d7c4e05c93d27b4410882cd33f2263
JoseTorquato <jltorquato12@gmail.com>
2022-06-06 21:27:15 -0300

    feat: create class base for repository

---
+++ src/models/repository.py
@@ -0,0 +1,19 @@
+from models.interfaces.repository_interface import RepositoryInterface
+
+
+class Repository(RepositoryInterface):
+    def migrate(self):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def select(self):
+        pass
+    
