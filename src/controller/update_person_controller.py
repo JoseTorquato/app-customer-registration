1b2eec8981fc132835b4f6fa35d316b4c618fa93
JoseTorquato <jltorquato12@gmail.com>
2022-06-18 23:47:58 -0300

    config: settings black test

--- src/controller/update_person_controller.py
+++ src/controller/update_person_controller.py
@@ -4,8 +4,8 @@ from src.models.interfaces.repository_interface import RepositoryInterface
 
 
 class UpdateCustomerRegisterController:
-    def __init__(self, repository: Type[RepositoryInterface]) -> None:
-        self.__repository = repository 
+    def __init__(self, repository: Type[RepositoryInterface]) -> Dict:
+        self.__repository = repository
 
-    def process(self, data: Dict, db_name: str) -> any:
-        return self.__repository.update(data, db_name)    
+    def process(self, data: Dict, db_name: str) -> Dict:
+        return self.__repository.update(data, db_name)
